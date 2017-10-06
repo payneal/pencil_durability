5afc6bbaf05bbcf663c1ad83d735361e4f5fcc34
Ali Payne <me@alipayne.com>
2017-10-06 16:37:16 -0400

    test passed writting to file

--- pencil_durability.js
+++ pencil_durability.js
@@ -1,7 +1,16 @@
+var fs = require('fs')
 function Pencil(text_file, text_input) {
-
+    var self = this
     this.text_file = text_file;
     this.text_input = text_input;
+    write_to_paper(self)
+
+    function write_to_paper(self){
+        var logger = fs.createWriteStream(
+            self.text_file, {flags: 'w' })
+        logger.write(self.text_input);
+        logger.end();
+    }
 
     this.written_text = function() {
         return this.text_input;

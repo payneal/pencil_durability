cc5708c6569863c7818c6ab85daf36d4daa0a94c
Ali Payne <me@alipayne.com>
2017-10-06 16:36:29 -0400

    changed test so that it passes correct location of file

--- test/test_pencil_durability.js
+++ test/test_pencil_durability.js
@@ -2,11 +2,12 @@
 const assert = require('chai').assert;
 const Pencil = require('../pencil_durability');
 const fs = require('fs')
-
+var path = require("path");
 
 get_file_text = function(file_name) {
     return new Promise(function (resolve, reject) {
-        fs.readFile('./files/' + file_name, 'utf8', function (err,data) {
+        fs.readFile('./files/' + file_name, 'utf8', 
+                function (err,data) {
             if (err) {
                 reject(console.log(err));
             }
@@ -17,9 +18,10 @@ get_file_text = function(file_name) {
 
 describe("Pencil Durability", function(){
     it("should be able to write to a piece of paper", function(){
-        let pencil = new Pencil('../blank_paper.txt', "a");
+        let path_to_file = path.resolve('files/blank_paper.txt');
+        let pencil = new Pencil(path_to_file, "a");
         var text_written = pencil.written_text()
-        return get_file_text('blank_paper.txt')
+        return get_file_text("blank_paper.txt")
             .then((expected_text) => {
                 let result = text_written.trim();
                 let expected = expected_text.trim();

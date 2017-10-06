bb7342743cb98620af50780054b6410d3c3895b0
Ali Payne <me@alipayne.com>
2017-10-06 18:47:26 -0400

    refactor code

--- pencil_durability.js
+++ pencil_durability.js
@@ -1,19 +1,33 @@
 var fs = require('fs')
-function Pencil(text_file, text_input) {
+function Pencil() {
+    // private vars
     var self = this
-    this.text_file = text_file;
-    this.text_input = text_input;
-    write_to_paper(self)
 
-    function write_to_paper(self){
-        var logger = fs.createWriteStream(
-            self.text_file, {flags: 'w' })
-        logger.write(self.text_input);
-        logger.end();
+    //public vars
+    this.file_location = null;
+    this.text = ""
+
+    //private functions
+    function set_location_and_text(self, file_location, text_to_write) {
+        self.file_location = file_location;
+        self.text = text_to_write; 
     }
 
-    this.written_text = function() {
-        return this.text_input;
+    function write_to_file(self){
+        return new Promise(function (resolve, reject) {
+            var logger = fs.createWriteStream(
+                self.file_location, {flags: 'a' })
+            logger.write(self.text);
+            resolve();
+        }).catch((err) => {
+            reject();
+        });
+    }
+
+    // public functions
+    this.write = function(file_location, text_to_write) {
+        set_location_and_text(self, file_location, text_to_write);
+        write_to_file(self)
     }
 }
 

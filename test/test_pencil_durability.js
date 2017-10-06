5c1a11feabf41751ab5d5e7d91299121fe86574d
Ali Payne <me@alipayne.com>
2017-10-06 19:00:15 -0400

    test passed adding multiple lines

--- test/test_pencil_durability.js
+++ test/test_pencil_durability.js
@@ -26,8 +26,6 @@ clear_file = function(file_name) {
     });
 }
 
-
-
 describe("Pencil Durability", function(){
 	
 	describe("Pencil writing", function(){	
@@ -36,15 +34,29 @@ describe("Pencil Durability", function(){
 		});
 
 		it("should be able to write to a piece of paper", function(){
-			let path_to_file = path.resolve('files/blank_paper.txt');
-			let pencil = new Pencil()
-			pencil.write(path_to_file, "a");
+            let pencil = new Pencil();
+			pencil.write( path.resolve('files/blank_paper.txt'), "a");
 			return get_file_text("blank_paper.txt")
 				.then((result) => {
 					assert.equal(result.trim(), "a");
 				})
 		});
 
+        it("pencil should be able to append to paper", function(){
+            let pencil = new Pencil(); 
+            pencil.write(            
+                path.resolve('files/blank_paper.txt'),
+                "She sells sea shells");
+            pencil.write(            
+                path.resolve('files/blank_paper.txt'),
+                " down by the sea shore");
+            return get_file_text("blank_paper.txt")
+                .then((result) => {
+                    assert.equal(
+                        result.trim(),
+                        "She sells sea shells down by the sea shore"
+                    )
+                })
+        });
 	});
-
 });

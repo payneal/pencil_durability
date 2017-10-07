62445c0af6862cf9891b813ef76161ede1ce7ff2
Ali Payne <me@alipayne.com>
2017-10-06 23:18:41 -0400

    test passed that 4 durability with Text => Tex

--- pencil_durability.js
+++ pencil_durability.js
@@ -1,9 +1,10 @@
 var fs = require('fs')
-function Pencil() {
+function Pencil(durability=100) {
     // private vars
     var self = this
 
     //public vars
+    this.durability = durability;
     this.file_location = null;
     this.text = ""
 
@@ -15,20 +16,39 @@ function Pencil() {
 
     function write_to_file(self){
         return new Promise(function (resolve, reject) {
-            var logger = fs.createWriteStream(
-                self.file_location, {flags: 'a' })
-            logger.write(self.text);
-            resolve();
-        }).catch((err) => {
-            reject();
-        });
+            for (var i = 0; i < self.text.length; i++) {
+				if (self.text[i] === self.text[i].toUpperCase() &&
+					 	self.text[i] !== self.text[i].toLowerCase()) { 
+					// its upper case
+					if (self.durability - 2 >= 0) { 
+						fs.appendFileSync(self.file_location, self.text[i]);
+						self.durability -= 2;
+ 					} else { 
+						fs.appendFileSync(self.file_location, " ");
+					}
+				} else {
+					// its lower case
+					if (self.durability - 1 >= 0) {
+						fs.appendFileSync(self.file_location, self.text[i])
+						self.durability -= 1;
+					} else {
+						fs.appendFileSync(self.file_location, " ");
+					}	 					
+            	}
+			}        
+			
+			resolve();
+        
+		}).catch((err) => {
+			throw err;
+		});
     }
 
     // public functions
     this.write = function(file_location, text_to_write) {
         set_location_and_text(self, file_location, text_to_write);
-        write_to_file(self)
-    }
+		write_to_file(self);
+	}
 }
 
 module.exports = Pencil

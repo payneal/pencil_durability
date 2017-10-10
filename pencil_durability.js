28cb14a0e7f5e4f53dcedc1b56a699bea2c5f1a3
Ali Payne <me@alipayne.com>
2017-10-10 17:10:10 -0400

    refactor

--- pencil_durability.js
+++ pencil_durability.js
@@ -1,14 +1,40 @@
 var fs = require('fs')
 function Pencil(durability=100, length=100) {
     // private vars
-    var self = this
+    var self = this;
 
     //public vars
     this.durability = durability;
     this.original_durability = durability;
     this.length = length;
     this.file_location = null;
-    this.text = ""
+    this.text = "";
+
+    // public functions
+    this.write = function(file_location, text_to_write) {
+        set_location_and_text(self, file_location, text_to_write);
+		write_to_file(self);
+    }
+
+    this.sharpen = function() {
+        if (this.length > 0) { 
+            this.durability = this.original_durability;
+            length -= 1;
+        }
+    }
+
+    this.erase = function(word_to_erase) {
+		return Promise.resolve()
+		    .then(() => all_filetext_to_array_of_chars(self))
+            .then(array_of_chars => 
+                find_string_in_text(self, array_of_chars, word_to_erase))
+            .then(([result, array_of_chars]) => { 
+                if (result) erase_found_word(self, result, array_of_chars);
+            })       
+            .catch(err => {
+                throw err;
+		    });
+	}
 
     //private functions
     function set_location_and_text(self, file_location, text_to_write) {
@@ -17,14 +43,9 @@ function Pencil(durability=100, length=100) {
     }
 
     function write_to_file(self){
-        return new Promise(function (resolve, reject) {
-            for (var i = 0; i < self.text.length; i++) {
-                insert_char_to_file(self, i);
-            }        	
-			resolve();  
-		}).catch((err) => {
-			throw err;
-		});
+        for (var i = 0; i < self.text.length; i++) {
+            insert_char_to_file(self, i);
+        }        	
     }
 
     function insert_char_to_file(self, char_idx) { 
@@ -46,75 +67,44 @@ function Pencil(durability=100, length=100) {
     function make_durability_change(self, amount, char_idx) {
         if (self.durability - amount >= 0) {
             fs.appendFileSync(self.file_location, self.text[char_idx]);
-            if (self.text[char_idx] !== ' ') self.durability -= amount
+            if (self.text[char_idx] !== ' ') self.durability -= amount;
         }  
     }
 
-    // public functions
-    this.write = function(file_location, text_to_write) {
-        set_location_and_text(self, file_location, text_to_write);
-		write_to_file(self);
-    }
-
-    this.sharpen = function() {
-        if (this.length > 0) { 
-            this.durability = this.original_durability;
-            length -= 1;
+    function find_string_in_text(self, array_of_chars, word_looking_for) {
+        for (letter =0; letter<array_of_chars.length; letter++){
+            if (array_of_chars[letter] === word_looking_for[0]){
+                var location = find_word(self, array_of_chars, letter, word_looking_for)
+                if (location) return [location, array_of_chars];
+            }
         }
     }
 
-    this.erase = function(word_to_erase) {
-		return Promise.resolve()
-		.then(() => all_filetext_to_array_of_chars(self))
-		.then(array_of_chars => {
-			for (i=0; i<array_of_chars.length; i++){
-				if (array_of_chars[i] === word_to_erase[0]){
-					var response = verify_match(
-						self, array_of_chars, i, word_to_erase)
-					if (response !== false) {
-						return response;
-					}
-				}
-			}
-		})
-		.then(result => rewrite_to_paper(self, result))
-		.then( ahh => {
-			return ahh;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-	}
-
-	function verify_match(self, array_of_chars, i, word_to_erase) {
-		
-		var erase_spaces = [];
-
-		for (var index = 0; index < word_to_erase.length; index++) {
-			if ( i - index < 0 ||  array_of_chars[i - index] !== word_to_erase[index]) {
-				erase_spaces = [];
-				return false;
+    function find_word(self, array_of_chars, i, word_looking_for) {
+        location = []
+        for (var index = 0; index < word_looking_for.length; index++) {
+			if ( i - index < 0 ||  array_of_chars[i - index] !== word_looking_for[index]) {
+                return false;
 			} else {
-				erase_spaces.push(i - index)
-			}
-			
+				location.push(i - index);
+			}	
+        }
+        return location;
+    }
+
+	function erase_found_word(self, location, array_of_chars) {   
+		for(var i =0; i < location.length; i++) {
+			array_of_chars[location[i]] = " ";
 		}
-		for(var i =0; i < erase_spaces.length; i++) {
-			array_of_chars[erase_spaces[i]] = " "
-		}
-		return array_of_chars.reverse().toString().replace(/\,/g, "");
+		rewrite_to_paper(self, array_of_chars.reverse().toString().replace(/\,/g, ""));
 	}
 
    	function rewrite_to_paper(self, array_of_chars) { 	
 		return new Promise(function (resolve, reject) { 
-	
 			fs.writeFile(self.file_location , array_of_chars, function(err) {
-    			if(err) {
-					throw err;
-					reject(err);
-    			}
-				resolve(true);
-			});
+    			if(err) throw err;
+				resolve();
+			})
 		});
 	}
 
@@ -126,10 +116,9 @@ function Pencil(durability=100, length=100) {
 					reject(err);
 				}
 				resolve(data.toString().split("").reverse());
-			}); 
+			}) 
 		});	
     }
-
 }
 
 module.exports = Pencil

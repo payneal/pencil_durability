2ba68a3c05b3630f500827044c1962e724563d97
Ali Payne <me@alipayne.com>
2017-10-10 15:16:36 -0400

    passed test erase last occurance of certain word

--- pencil_durability.js
+++ pencil_durability.js
@@ -62,6 +62,74 @@ function Pencil(durability=100, length=100) {
             length -= 1;
         }
     }
+
+    this.erase = function(word_to_erase) {
+		return Promise.resolve()
+		.then(() => all_filetext_to_array_of_chars(self))
+		.then(array_of_chars => {
+			for (i=0; i<array_of_chars.length; i++){
+				if (array_of_chars[i] === word_to_erase[0]){
+					var response = verify_match(
+						self, array_of_chars, i, word_to_erase)
+					if (response !== false) {
+						return response;
+					}
+				}
+			}
+		})
+		.then(result => rewrite_to_paper(self, result))
+		.then( ahh => {
+			return ahh;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	}
+
+	function verify_match(self, array_of_chars, i, word_to_erase) {
+		
+		var erase_spaces = [];
+
+		for (var index = 0; index < word_to_erase.length; index++) {
+			if ( i - index < 0 ||  array_of_chars[i - index] !== word_to_erase[index]) {
+				erase_spaces = [];
+				return false;
+			} else {
+				erase_spaces.push(i - index)
+			}
+			
+		}
+		for(var i =0; i < erase_spaces.length; i++) {
+			array_of_chars[erase_spaces[i]] = " "
+		}
+		return array_of_chars.reverse().toString().replace(/\,/g, "");
+	}
+
+   	function rewrite_to_paper(self, array_of_chars) { 	
+		return new Promise(function (resolve, reject) { 
+	
+			fs.writeFile(self.file_location , array_of_chars, function(err) {
+    			if(err) {
+					throw err;
+					reject(err);
+    			}
+				resolve(true);
+			});
+		});
+	}
+
+	function all_filetext_to_array_of_chars(self){ 
+    	return new Promise(function (resolve, reject) { 
+			fs.readFile(self.file_location, function(err, data) {
+				if(err)  {
+					throw err;
+					reject(err);
+				}
+				resolve(data.toString().split("").reverse());
+			}); 
+		});	
+    }
+
 }
 
 module.exports = Pencil

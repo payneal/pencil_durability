97866fd88b3456c0e6036a6d4c857cb5be392c93
Ali Payne <me@alipayne.com>
2017-10-11 16:12:12 -0400

    refactor

--- pencil_durability.js
+++ pencil_durability.js
@@ -26,29 +26,28 @@ function Pencil(durability=100, length=100, eraser_durability=100) {
     }
 
     this.erase = function(word_to_erase) {
-        return Promise.resolve()
-		    .then(() => all_filetext_to_array_of_chars(self))
+        return convert_filetext_to_array(self)
             .then(array_of_chars => find_string_in_text(self, array_of_chars, word_to_erase))
-            .then(([result, array_of_chars]) => {
-                if (result) erase_found_word(self, result, array_of_chars);
-            })       
-            .catch(err => {
-                throw err;
-		    });
+            .then(([result, array_of_chars]) => erase_text_if_it_exist(self, result, array_of_chars))
+            .catch(err => {throw err});
 	}
 
     this.edit = function(word_to_add) {
-        if (self.edit_location !== []) { 
-            return Promise.resolve()
-                .then(() => all_filetext_to_array_of_chars(self))
-                .then( array_of_chars => edit_erase_text(self, array_of_chars, word_to_add))
-                .catch( err => {
-                    throw err;
-                });
-        }
+		return convert_filetext_to_array(self)
+			.then( array_of_chars => edit_erase_text(self, array_of_chars, word_to_add))
+			.catch( err => {throw err});
     }
 
     //private functions
+	function erase_text_if_it_exist(self, result, array_of_chars) {
+		if (result) erase_found_word(self, result, array_of_chars);
+	}
+	
+	function convert_filetext_to_array(self) {
+		return Promise.resolve()
+        	.then(() => all_filetext_to_array_of_chars(self))
+	}
+
     function set_location_and_text(self, file_location, text_to_write) {
         self.file_location = file_location;
         self.text = text_to_write; 
@@ -63,9 +62,7 @@ function Pencil(durability=100, length=100, eraser_durability=100) {
     function insert_char_to_file(self, char_idx) { 
         if (check_if_letter_captial(self, char_idx)) {
             make_durability_change(self, 2, char_idx);
-        } else {
-            make_durability_change(self, 1, char_idx);
-        } 
+        } else make_durability_change(self, 1, char_idx); 
     }
 
     function check_if_letter_captial(self, char_idx) {
@@ -79,39 +76,43 @@ function Pencil(durability=100, length=100, eraser_durability=100) {
     function make_durability_change(self, amount, char_idx) {
         if (self.durability - amount >= 0) {
             fs.appendFileSync(self.file_location, self.text[char_idx]);
-            if (self.text[char_idx] !== ' ') self.durability -= amount;
-        }  
-    }
+			subtract_durability_if_char_written_requires_led(amount, char_idx);
+		}
+	}
 
+	function subtract_durability_if_char_written_requires_led(amount, char_idx){
+    	if (self.text[char_idx] !== ' ') self.durability -= amount;
+	}
+    
     function find_string_in_text(self, array_of_chars, word_looking_for) {
-        for (letter =0; letter<array_of_chars.length; letter++){
-            if (array_of_chars[letter] === word_looking_for[0]){
-                var location = find_word(self, array_of_chars, letter, word_looking_for)
-                if (location) return [location, array_of_chars];
-            }
+        for ( char_idx =0; char_idx < array_of_chars.length; char_idx++ ){
+			var location = verify_first_char_found_of_search_word(
+				self, array_of_chars, char_idx, word_looking_for);
+			if (location) return [location, array_of_chars];
         }
         return [null, null];
     }
 
+	function verify_first_char_found_of_search_word(self, array_of_chars, char_idx, word_looking_for) {
+		if (array_of_chars[char_idx] === word_looking_for[0]){
+			return find_word(self, array_of_chars, char_idx, word_looking_for)
+		}
+		return false;	
+	}
+	
     function find_word(self, array_of_chars, i, word_looking_for) {
         var location = []
         for (var index = 0; index < word_looking_for.length; index++) {
-            if (check_for_find_word(self, array_of_chars, i, index, word_looking_for)) {
+            if (check_for_find_word(self, array_of_chars, i, index, word_looking_for))
                 location.push(i - index);
-            }
-            else { 
-                return false;
-            }
+            else return false;
         }
         return location.reverse();
     }
 
     function check_for_find_word(self, array_of_chars, i, index, word_looking_for) { 
-        if ( i - index < 0 ||  array_of_chars[i - index] !== word_looking_for[index]) {
-            return false;
-        } else {
-            return true;
-        }	
+        if ( i - index < 0 ||  array_of_chars[i - index] !== word_looking_for[index]) return false;
+        return true;	
     }
 
     function erase_found_word(self, location, array_of_chars) { 
@@ -144,32 +145,36 @@ function Pencil(durability=100, length=100, eraser_durability=100) {
 	function all_filetext_to_array_of_chars(self){ 
     	return new Promise(function (resolve, reject) { 
 			fs.readFile(self.file_location, function(err, data) {
-				if(err)  {
-					throw err;
-					reject(err);
-				}
+				if(err) throw err;
 				resolve(data.toString().split("").reverse());
 			}) 
 		});	
     }
 
-    function edit_erase_text(self, array_of_chars, word_to_add) {
+    function set_up_for_edit_erase_text(self) {
         self.edit_location.reverse();
-        var last_char = self.edit_location[self.edit_location.length-1];
-        
-        for(var i=0; i< word_to_add.length; i++) {
-            if (self.edit_location[i] == null) {
-                last_char -=  1;
-                if (array_of_chars[last_char] !== " ") {
-                    array_of_chars[last_char] = '@'; 
-                } else { 
-                    array_of_chars[last_char] = word_to_add[i]; 
-                }    
-            } else {
-                array_of_chars[self.edit_location[i]] = word_to_add[i];
-            }
-        }
-        self.edit_location = []; 
+        return self.edit_location[self.edit_location.length-1];
+    }
+
+    function edit_for_overlap(self, array_of_chars, last_char, index, word_to_add) { 
+		if (array_of_chars[last_char] !== " ") array_of_chars[last_char] = '@'; 
+       	else array_of_chars[last_char] = word_to_add[index]; 
+		return {array_of_chars, last_char};
+    }
+
+	function loop_through_word_to_add(self, last_char, array_of_chars, word_to_add) { 
+		for(var i=0; i< word_to_add.length; i++) {
+			if (self.edit_location[i] == null) {
+				var { array_of_chars, last_char } = edit_for_overlap(
+					self, array_of_chars, last_char -1,i, word_to_add); 
+			} else array_of_chars[self.edit_location[i]] = word_to_add[i];
+		}
+		return array_of_chars;
+    }
+
+    function edit_erase_text(self, array_of_chars, word_to_add) {
+        var last_char = set_up_for_edit_erase_text(self); 
+        array_of_chars = loop_through_word_to_add(self, last_char, array_of_chars, word_to_add)
         rewrite_to_paper(self, array_of_chars.reverse().toString().replace(/\,/g, ""))
     }
 }
